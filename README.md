695b55da230772a8b6e36496906d7f0b38b8af65
Renaldy Hidayat <renaldyhidayatt@gmail.com>
2026-08-21 23:42:09 +0700

    feat: initialize Spring Boot project structure with Maven wrapper and H2 configuration

---
+++ README.md
@@ -0,0 +1,1 @@
+# Splil Bill API
---
+++ src/test/java/com/renaldyhidayatt/split_bill_api/SplitBillApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renaldyhidayatt.split_bill_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitBillApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
